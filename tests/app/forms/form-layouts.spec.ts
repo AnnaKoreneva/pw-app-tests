cca1f8b09c2170f87bc221ed4b6bfd85d4c7c52f
AnnaKoreneva <anna.koreneva1409@gmail.com>
2024-09-25 13:42:28 -0700

    form_layout_refactoring

--- tests/app/forms/form-layouts.spec.ts
+++ tests/app/forms/form-layouts.spec.ts
@@ -1,35 +1,35 @@
 import test from "@playwright/test";
-import { FormLayout } from "../../../src/app/page/forms/form-layouts/form-layout";
 import { InlineForm } from "../../../src/app/page/forms/form-layouts/inline-form";
+import { UsingTheGrid } from "../../../src/app/page/forms/form-layouts/using-the-grid";
+import { iForms } from "../../../data/forms/iForms";
+
+let formData: iForms = {
+  email: "test@test.com",
+  password: "Pass123",
+  userName: "AnnaK",
+  firstName: "Anna",
+  lastName: "K",
+  radios: "Option 1",
+  checkBox: true,
+};
 
 test.beforeEach(async ({ page }) => {
     await page.goto("http://localhost:4200/pages/forms/layouts");
 })
 
 test.describe("using the grid form", () => {
-
-   test("email fill", async ({ page }) => {
-     const formLayout = new FormLayout(page);
-     await formLayout.fillUsingGridEmail("test");
-     await formLayout.checkRadioButton();
-     await formLayout.clickSignIn();
-   }); 
-    
+  test("sign in", async ({ page }) => {
+    const usingTheGrid = new UsingTheGrid(page);
+    await usingTheGrid.signIn(formData);
+    await usingTheGrid.checkTheSignInFormData(formData);
+  })
 })
 
 test.describe("inline form", () => {
-
   test("email fill", async ({ page }) => {
     const inlineForm = new InlineForm(page);
-    await inlineForm.submitInlineForm('Anna Koreneva', 'test@test.com')
-    await inlineForm.checkInputValues("Anna Koreneva", "test@test.com");
+    await inlineForm.submitInlineForm(formData);
+    await inlineForm.checkInputValues(formData);
   });
     
-});
-
-test.describe("basic form", () => {
-  test("click sign in", async ({ page }) => {
-    const formLayout = new FormLayout(page);
-    await formLayout.clickSignInBasicForm()
-  });
 });
