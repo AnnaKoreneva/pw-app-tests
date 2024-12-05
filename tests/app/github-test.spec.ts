83df8349b0fc0b6e87ed345f669e0ae5a1a97b10
AnnaKoreneva <anna.koreneva1409@gmail.com>
2024-12-05 14:42:43 -0800

    Test

--- tests/app/github-test.spec.ts
+++ tests/app/github-test.spec.ts
@@ -1,4 +1,4 @@
 import test from '@playwright/test';
 test('github test without env variable', async ({ page }) => {
-    await page.goto('http://localhost:4200/');
+    await page.goto('https://rozetka.com.ua');
 });
